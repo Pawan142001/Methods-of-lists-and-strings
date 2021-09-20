6dc2e6ca1d29516821a7742d94d20fc5f9a16a49
Pawan142001 <90554117+Pawan142001@users.noreply.github.com>
2021-09-20 13:13:05 +0530

    Update listmethods.py

--- listmethods.py
+++ listmethods.py
@@ -52,3 +52,89 @@ print(bikes)
 trees = ["Mango","Guava","Apple"]
 trees.reverse()
 print(trees)
+
+String Methods :-
+    
+txt = "game is over"
+x = txt.capitalize()
+print(x)
+
+txt = "Python is a Programming Language"
+x = txt.casefold()
+print(x)
+
+txt = "Python"
+x = txt.center(20)
+print(x)
+
+txt = "I like pizza,pizza is my favorite;izza pizza jizza"
+x = txt.count("pizza")
+print(x)
+
+txt = "My name is Påwãn "
+print(txt.encode(encoding="ascii",errors="backslashreplace"))
+print(txt.encode(encoding="ascii",errors="ignore"))
+print(txt.encode(encoding="ascii",errors="namereplace"))
+print(txt.encode(encoding="ascii",errors="replace"))
+print(txt.encode(encoding="ascii",errors="xmlcharrefreplace"))
+
+txt = "Namaste,aapka swagat hai"
+x = txt.endswith(".")
+print(x)
+
+txt = "P\ta\tw\ta\tn"
+x =  txt.expandtabs(2)
+print(x)
+
+txt = "Hello,welcome to india"
+x = txt.find("welcome")
+print(x)
+
+txt = "Hello,welcome to india"
+x = txt.find("e",5,8)
+print(x)
+
+txt = "For only {price:.2f} rupees!"
+print(txt.format(price = 200))
+
+txt = "Hello, welcome to india."
+x = txt.index("welcome")
+print(x)
+
+txt = "Important234"
+x = txt.isalnum()
+print(x)
+
+txt = "Important234"
+x = txt.isalpha()
+print(x)
+
+txt = "\u0033" #unicode for 3
+x = txt.isdecimal()
+print(x)
+
+txt = "2498"
+x = txt.isdigit()
+print(x)
+
+a = "MyFile"
+b = "Open002"
+c = "2bars"
+d = "my demo"
+print(a.isidentifier())
+print(b.isidentifier())
+print(c.isidentifier())
+print(d.isidentifier())
+
+txt = "hello guys!"
+x = txt.islower()
+print(x)
+
+txt = "565543"
+x = txt.isnumeric()
+print(x)
+
+
+
+
+
